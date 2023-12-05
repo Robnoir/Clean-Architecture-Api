bea0a07a79cc4428a7b3cdbc2182f1184ae4fda5
Robert Svart <robnoir@Roberts-MacBook-Air.local>
2023-12-05 19:49:30 +0100

    trying to include all the validation files

---
+++ Application/Validators/Dog/DogValidator.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using Application.Dtos;
+using Application.Queries.Dogs.GetAll;
+using Domain.Models;
+using FluentValidation;
+
+public class DogValidator : AbstractValidator<DogDto>
+{
+    public DogValidator()
+    {
+        RuleFor(dog => dog.Name)
+        .NotEmpty().WithMessage("Dog Name can not be empty")
+        .NotNull().WithMessage("Dog name can not be Null");
+
+    }
+  
+
+}
