0a403cd848089d26d22fa6752bffd14b24c36e76
Robert Svart <robnoir@Roberts-MacBook-Air.local>
2023-12-11 14:14:55 +0100

    DatabaseConnection done

---
+++ Infrastructure/Database/DatabaseSeedHelper.cs
@@ -0,0 +1,40 @@
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database.DatabaseHelpers
+{
+    public static class DatabaseSeedHelper
+    {
+        public static void SeedData(ModelBuilder modelBuilder)
+        {
+            Seed(modelBuilder);
+            // Add more methods for other entities as needed
+            
+        }
+
+        private static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dog>().HasData(
+            new Dog { Id = Guid.NewGuid(), Name = "Björn"},
+            new Dog { Id = Guid.NewGuid(), Name = "Patrik"},
+            new Dog { Id = Guid.NewGuid(), Name = "Alfred"},
+            new Dog { Id = new Guid("12345678-1234-5678-1234-567812345678"), Name = "TestDogForUnitTests"},
+            new Dog { Id = new Guid("12345678-1234-5678-1234-567812345679"), Name = "TestDeleteDog" }
+            );
+
+            modelBuilder.Entity<Cat>().HasData(
+            new Cat { Id = Guid.NewGuid(), Name = "Nugget", LikesToPlay = true},
+            new Cat { Id = Guid.NewGuid(), Name = "SmallMac", LikesToPlay = true},
+            new Cat { Id = Guid.NewGuid(), Name = "Avocado", LikesToPlay = false},
+            new Cat { Id = new Guid("12345678-1234-5678-1234-567812345610"), Name = "TestCatForUnitTests",LikesToPlay =true},
+            new Cat { Id = new Guid("12345678-1234-5678-1234-567812345611"), Name = "TestDeleteCat" , LikesToPlay = true}
+
+            );
+
+            
+        }
+
+
+        
+    }
+}
--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Infrastructure.Database;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Infrastructure
 {
@@ -8,7 +10,14 @@ namespace Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<MockDatabase>();
+            services.AddDbContext<RealDatabase>(options =>
+            {
+                options.UseMySql("server=localhost;port=3306;user=root;password=Robert123;database=RealDB",
+                         new MySqlServerVersion(new Version(8, 2, 0)));
+            });
             return services;
+            
         }
+
     }
 }
