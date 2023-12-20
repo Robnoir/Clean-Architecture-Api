2911266afb52a08518d7b5a9acf98d0d7d89bf50
Robert Svart <robert.svart@student.nbi-handelsakademin.se>
2023-12-20 18:19:40 +0100

    Implemented many to many relations/deleted all migrations from before and made a new one

--- Infrastructure/Migrations/20231220170307_newnewmig2.Designer.cs
+++ Infrastructure/Migrations/20231220170307_newnewmig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(RealDatabase))]
-    partial class RealDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20231220170307_newnewmig2")]
+    partial class newnewmig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
