e67927dae3be41c616aaa4c5a79ef70926548837
Robert Svart <robert.svart@student.nbi-handelsakademin.se>
2024-01-04 14:07:13 +0100

    commit to pull

--- Application/Dtos/UserAnimalDto.cs
+++ Application/Dtos/UserAnimalDto.cs
@@ -11,10 +11,7 @@ namespace Application.Dtos
     {
         public Guid UserId { get; set; }
         public string Username { get; set; }
-
         public Guid AnimalId { get; set; }
-
-
         public List<DogDto> Dogs { get; set; }
         public List<CatDto> Cats { get; set; }
         public List<BirdDto> Birds { get; set; }
